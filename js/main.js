729b4ba0c22f458387d9d08ea1654a4e130bd915
ihor-voznyi <haiku.kiev@gmail.com>
2024-05-13 20:47:10 +0300

    glob upd

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const headerNavBtn = document.querySelector('.header-nav--btn');
 const mobailCloseBtn = document.querySelector('.mobail-close-btn');
 const mobailMenu = document.querySelector('.mobail-menu');
 
-const orderBtn = document.querySelector('.button-box');
+const orderBtn = document.querySelector('.hero-btn');
 const modalCloseBtn = document.querySelector('.modal-close-btn');
 const modalOverlay = document.querySelector('.modal-overlay');
 const modalWindow = document.querySelector('.modal-window');
